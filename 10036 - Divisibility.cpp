8e7aff4e524416cb2f3e8b6e36b519afe6e75fde
Tawraat <tawraat0187@gmail.com>
2021-09-24 23:34:25 +0600

    solved

---
+++ 10036 - Divisibility.cpp
@@ -0,0 +1,134 @@
+#include   <bits/stdc++.h>
+
+#define    nline        cout << '\n';
+#define    lastof(s)    s.size() - 1
+#define    all(x)       x.begin(), x.end()
+#define    pb           push_back
+#define    mp           make_pair
+#define    EPS          1e-9
+
+typedef    unsigned long long ull;
+typedef    long long ll;
+using      namespace std;
+
+void OJ(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+#ifndef ONLINE_JUDGE
+
+auto debug(){ return cout << endl,0; }
+template<typename Arg,typename... Args>
+auto debug(Arg elem,Args... elems){
+    cout << "debugging: " << elem << ' ';
+    // cout << endl;
+    return debug(elems...);
+}
+
+template<typename T>
+void printVec(T& v){
+    cout << "container : [ ";
+    for(auto &it: v) cout << it << ' ';
+    cout << "]\n";
+}
+
+#endif
+
+const ll mod = 998244353;
+const ll maxN = 10000000;
+
+ll bigMod(ll a, ll b, ll m){
+    ll ans = 1;
+    a = a % m;
+
+    while(b){
+        if(b & 1)
+            ans = (ans * a) % m;
+        
+        a = (a*a) % m;
+        b /= 2;
+    }
+    return ans;
+}
+
+ll MOD(ll num){ return ((num%mod + mod)%mod); }
+
+ll modAdd(ll a, ll b){ return MOD(MOD(a) + MOD(b)); }
+
+ll modSub(ll a, ll b){ return MOD(MOD(a) - MOD(b)); }
+
+ll modMul(ll a, ll b){ return MOD(MOD(a) * MOD(b)); }
+
+ll modDiv(ll a, ll b){ return modMul(a, bigMod(b,mod-2, mod)); }
+    
+ll Lcm(ll x, ll y) { return x * (y / __gcd(x, y)); }
+    
+ll Sum(ll n) { return (n * (n + 1)) / 2; }
+    
+ll logAB(ll a, ll b) { return log(a) / log(b); }
+    
+ll ceilAB(ll a, ll b) { return (a + b - 1) / b; }
+    
+ll chartoint(char ch) { return ch - '0'; }
+    
+bool coPrime(ll a, ll b) { return __gcd(a,b) == 1; }
+
+bool isEqual(double a, double b) { return abs(a-b) < EPS; }
+
+bool isGreater(double a, double b) { return a >= b + EPS; }
+
+bool isGreaterEqual(double a, double b) { return a > b - EPS; }
+
+int n, k;
+vector<ll> v(10000+10);
+ll dp[10000+3][103];
+
+ll MOD(ll num, int k){ return ((num%k + k)%k); }
+
+int divisible(int pos, ll m){
+    if(pos == n)                        // base case
+        return m == 0;
+
+    if(dp[pos][m] != -1)                // return if already calculated
+        return dp[pos][m];
+    
+    return dp[pos][m] = (divisible(pos+1, MOD(m + v[pos], k)) || divisible(pos+1, MOD(m - v[pos], k)));
+}
+
+void solve(){
+    cin >> n >> k;
+
+    memset(dp, -1, sizeof(dp));
+
+    for(ll i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    if(divisible(0, 0))
+        cout << "Divisible";
+    else
+        cout << "Not divisible";
+}
+
+void TC(){
+    int Case = 0;
+    int t = 1;
+    cin >> t;
+    
+    while(t--){
+        // cout << "CASE " << ++Case << ": ";
+        solve();
+        nline;
+    }
+}
+    
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    OJ();
+    
+    // Start
+    TC();
+}
