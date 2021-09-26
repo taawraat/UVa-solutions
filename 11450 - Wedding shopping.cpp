1215aecb88fb05cc6ddba2e210cd798ababaf69e
Tawraat <tawraat0187@gmail.com>
2021-09-26 13:02:57 +0600

    solved

---
+++ 11450 - Wedding shopping.cpp
@@ -0,0 +1,156 @@
+#include   <bits/stdc++.h>
+
+#define    nline        cout << '\n';
+#define    lastof(s)    s.size() - 1
+#define    all(x)       x.begin(), x.end()
+#define    pb           push_back
+#define    mp           make_pair
+#define    EPS          1e-9
+
+typedef    unsigned long long ull;
+typedef    long long ll;
+using      namespace std;
+
+void OJ(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+#ifndef ONLINE_JUDGE
+
+auto debug(){ return cout << endl,0; }
+template<typename Arg,typename... Args>
+auto debug(Arg elem,Args... elems){
+    cout << "debugging: " << elem << ' ';
+    // cout << endl;
+    return debug(elems...);
+}
+
+template<typename T>
+void printVec(T& v){
+    cout << "container : [ ";
+    for(auto &it: v) cout << it << ' ';
+    cout << "]\n";
+}
+
+#endif
+
+const ll mod = 998244353;
+const ll maxN = 10000000;
+
+ll bigMod(ll a, ll b, ll m){
+    ll ans = 1;
+    a = a % m;
+
+    while(b){
+        if(b & 1)
+            ans = (ans * a) % m;
+        
+        a = (a*a) % m;
+        b /= 2;
+    }
+    return ans;
+}
+
+ll MOD(ll num){ return ((num%mod + mod)%mod); }
+
+ll modAdd(ll a, ll b){ return MOD(MOD(a) + MOD(b)); }
+
+ll modSub(ll a, ll b){ return MOD(MOD(a) - MOD(b)); }
+
+ll modMul(ll a, ll b){ return MOD(MOD(a) * MOD(b)); }
+
+ll modDiv(ll a, ll b){ return modMul(a, bigMod(b,mod-2, mod)); }
+    
+ll Lcm(ll x, ll y) { return x * (y / __gcd(x, y)); }
+    
+ll Sum(ll n) { return (n * (n + 1)) / 2; }
+    
+ll logAB(ll a, ll b) { return log(a) / log(b); }
+    
+ll ceilAB(ll a, ll b) { return (a + b - 1) / b; }
+    
+ll chartoint(char ch) { return ch - '0'; }
+    
+bool coPrime(ll a, ll b) { return __gcd(a,b) == 1; }
+
+bool isEqual(double a, double b) { return abs(a-b) < EPS; }
+
+bool isGreater(double a, double b) { return a >= b + EPS; }
+
+bool isGreaterEqual(double a, double b) { return a > b - EPS; }
+
+int money, garm;
+ll dp[210][30];
+vector<int> prices[30];
+
+void reset(){
+    for(ll i = 0; i < 25; i++){
+        prices[i].clear();
+    }
+}
+
+ll shop(int m, int g){
+    if(m > money) return -10000000000;      // boundary
+    
+    if(g == garm)                           // base case 
+        return m;
+
+    if(dp[m][g] != -1)                      // memoization
+        return dp[m][g];
+
+    int sz = prices[g].size();
+
+    for(ll i = 0; i < sz; i++)              // transition
+        dp[m][g] = max(dp[m][g], shop(m + prices[g][i], g+1));
+
+    return dp[m][g];
+}
+
+void solve(){
+    reset();
+    memset(dp, -1, sizeof(dp));
+
+    cin >> money >> garm;
+
+    int c, price;
+
+    for(ll i = 0; i < garm; i++){
+        cin >> c;
+
+        for(ll j = 0; j < c; j++){
+            cin >> price;
+
+            prices[i].pb(price);
+        }
+    }
+
+    int ans = shop(0, 0);
+
+    if(ans == -1)
+        cout << "no solution";
+    else
+        cout << ans;
+}
+
+void TC(){
+    int Case = 0;
+    int t = 1;
+    cin >> t;
+    
+    while(t--){
+        // cout << "CASE " << ++Case << ": ";
+        solve();
+        nline;
+    }
+}
+    
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    OJ();
+    
+    // Start
+    TC();
+}
